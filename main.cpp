d0b4f1c0970940db58e7cdb4c575c8710709942e
CoderXL1 <leo_xia@icloud.com>
2025-09-18 18:01:10 +0800

    Cleanup And Refactor Project Structure

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Created with VSCode on Ubuntu 22.04
+//  Created by Leo Xia
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, DXRMV!\n";
+    return 0;
+}
